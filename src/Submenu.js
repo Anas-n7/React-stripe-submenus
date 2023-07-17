81bc577fb7789a794aa40d9712f72d4bcb16d1de
Anas-n7 <anasvvr2002@gamil.com>
2023-07-17 15:05:55 +0530

    first commit

---
+++ src/Submenu.js
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
+
+const Submenu = () => {
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
+
+  const container = useRef(null);
+  const [columns, setColumns] = useState("col-2");
+
+  useEffect(() => {
+    setColumns("col-2");
+    const submenu = container.current; // nammal hover cheyyumbo active aayituula div kaanikkum
+    console.log(submenu);
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+
+    if (links.length === 3) {
+      setColumns("col-3");
+    }
+    if (links.length > 3) {
+      setColumns("col-4");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
+  return (
+    <aside
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { url, label, icon } = link;
+          return (
+            <a href={url} key={index}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Submenu;
